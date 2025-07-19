1dcfcaefa31f0aac0950b11b70e381746c8751af
Vedanth-R <vedanth.rao.v@gmail.com>
2025-07-18 21:40:36 -0700

    Updated home page with placeholder data and UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
     id ("com.google.gms.google-services")
 }
 
-
 android {
     namespace = "com.example.myapp"
     compileSdk = 34
@@ -15,7 +14,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.myapp"
-        minSdk = 25
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -41,6 +40,8 @@ android {
     }
 }
 
+
+
 dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
 
@@ -53,6 +54,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
     implementation("com.google.ai.client.generativeai:generativeai:0.7.0")
     implementation("com.google.guava:guava:31.0.1-android")
     implementation("org.reactivestreams:reactive-streams:1.0.4")
@@ -62,6 +64,7 @@ dependencies {
     implementation ("com.google.android.libraries.places:places:2.6.0")
     implementation ("com.android.volley:volley:1.2.1")
     implementation ("com.google.firebase:firebase-auth")
+    implementation ("co.yml:ycharts:2.1.0")
 
 
 
