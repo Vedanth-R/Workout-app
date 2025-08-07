e9d93cda78fc41b58763a7817d527b23b62dc634
Shaheer Ahmed <imshaheer01@gmail.com>
2025-08-06 17:48:03 -0700

    Changed dependencies 2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -87,9 +87,24 @@ dependencies {
     implementation (libs.ycharts)
 
     // Jetpack Compose
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.foundation)
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.runtime)
+    implementation(libs.androidx.ui.tooling.preview)
+    debugImplementation(libs.androidx.ui.tooling)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.compose.material3.material3)
+    implementation(libs.androidx.material.icons.extended)
+
+
 
     // Jetpack Compose BOM (manages versions)
-    implementation(platform(libs.compose.bom))
+    val composeBom = platform(libs.compose.bom)
+    implementation(composeBom)
+    testImplementation(composeBom)
+    androidTestImplementation(composeBom)
 
     // Core UI components
     implementation(libs.ui)
